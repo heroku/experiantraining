3351b409369edf7d5751e8449d5c74c3adca3ad3
Samuel Sharaf <samuel.sharaf@gmail.com>
2016-03-01 13:15:53 -0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,7 +20,7 @@ public class Main {
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
 
-    get("/hello", (req, res) -> "Hello World");
+    get("/hello", (req, res) -> "Hello Experian");
 
     get("/", (request, response) -> {
             Map<String, Object> attributes = new HashMap<>();
